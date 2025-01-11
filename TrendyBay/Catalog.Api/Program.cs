dade1fe87e326b9ab7f1e2322d9317d5e856d63f
Vladyslav Tkachenko <vt7098386@gmail.com>
2025-01-11 19:40:11 +0200

    Task-1_Init_Project: - Init project

---
+++ TrendyBay/Catalog.Api/Program.cs
@@ -0,0 +1,24 @@
+
+namespace Catalog.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+     
+            builder.Services.AddControllers();
+           
+
+            var app = builder.Build();
+
+  
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
